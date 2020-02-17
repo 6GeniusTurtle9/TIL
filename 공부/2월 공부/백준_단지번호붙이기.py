22bada8ee86c34832de00fdded327de93f5b92cf
6GeniusTurtle9 <ycg01082@naver.com>
2020-02-17 17:57:10 +0900

    2월 17일 공부

--- 공부/2월 공부/백준_단지번호붙이기.py
+++ 공부/2월 공부/백준_단지번호붙이기.py
@@ -1,11 +1,30 @@
 def dfs(i, j):
-    visited[i][j] = 1
-    # 주변이 방문을 했고 0일때 개수 저장
-
-N = int(input())
-house = [list(input()) for _ in range(N)]
-
+    global cnt
+    if int(house[i][j]) == 0 or visited[i][j] == 1:
+        return 0
+    else:
+        visited[i][j] = 1
+        cnt += 1
+        for k in range(4):
+            ni = i + di[k]
+            nj = j + dj[k]
+            if N-1 >= ni >=0 and N-1 >= nj >= 0:
+                if int(house[ni][nj]) == 1 and visited[ni][nj] == 0:
+                    if dfs(ni, nj) == 0:
+                        return cnt
+        return cnt
 di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
-
+N = int(input())
+house = [list(input())for _ in range(N)]
 visited = [[0]*N for _ in range(N)]
+home = []
+for i in range(N):
+    for j in range(N):
+        cnt = 0
+        if int(house[i][j]) == 1 and visited[i][j] == 0:
+            home.append(dfs(i, j))
+home.sort()
+print("{}".format(len(home)))
+for k in range(len(home)):
+    print(home[k], end = "\n")
--- 공부/2월 공부/백준_유기농배추.py
+++ 공부/2월 공부/백준_유기농배추.py
@@ -1,3 +1,23 @@
+# 백준에서 런타임 떠서
+import sys
+sys.setrecursionlimit(1500)
+
+
+def dfs(i, j):
+    if cabbage[i][j] != 1 or visited[i][j] != 0:
+        return 0
+    else:
+        visited[i][j] = 1
+        for t in range(4):
+            ni = i + di[t]
+            nj = j + dj[t]
+            if M-1 >= nj >= 0 and N-1 >= ni >= 0:
+                if cabbage[ni][nj] == 1 and visited[ni][nj] == 0:
+                    if dfs(ni, nj) == 0:
+                     return 1
+        return 1
+
+
 T = int(input())
 for tc in range(1, T+1):
     M, N, K = map(int, input().split())
@@ -6,13 +26,15 @@ for tc in range(1, T+1):
         nj, ni = map(int, input().split())
         cabbage[ni][nj] = 1
 
-    # 4방향으로 1이 이어진 횟수를 기록
     di = [0, 0, 1, -1]
     dj = [1, -1, 0, 0]
-    cnt = 0
+
     visited = [[0]*M for _ in range(N)]
+    bug = 0
+
     for i in range(N):
         for j in range(M):
             if cabbage[i][j] == 1 and visited[i][j] == 0:
-                
+                bug += dfs(i, j)
 
+    print(bug)
